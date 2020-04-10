5535c4e4e6d0540b397cd03af7aadf365faea030
uoogio <giovanni.buono21@gmail.com>
2020-04-10 12:17:43 +0200

    Mod

--- RIOT_dev/simulated-device/Device.py
+++ RIOT_dev/simulated-device/Device.py
@@ -6,8 +6,15 @@ import uuid
 import random
 import time
 import sys
+import json
+
+import ttn
+import paho.mqtt.client as mqtt
+'''
 sys.path.insert(1, 'lib')
+
 from  MQTTSNclient import Client
+'''
 # Using the Python Device SDK for IoT Hub:
 #   https://github.com/Azure/azure-iot-sdk-python
 # The sample connects to a device-specific MQTT endpoint on your IoT Hub.
@@ -17,8 +24,9 @@ from azure.iot.device import IoTHubDeviceClient, Message
 # The device connection string to authenticate the device with your IoT hub.
 # Using the Azure CLI:
 # az iot hub device-identity show-connection-string --hub-name {YourIoTHubName} --device-id MyNodeDevice --output table
-CONNECTION_STRING = "HostName=assignment001.azure-devices.net;DeviceId=device;SharedAccessKey=KFroCSyTGQz/eap9e4rOHna1VL5Mxg+awD84ygkkGE4="
-
+CONNECTION_STRING = "HostName=Assignment.azure-devices.net;DeviceId=MyPythonDevice;SharedAccessKey=51kvyuxG6g+IjXLWrPyTmkQSfmD9g0HCCxzVa3zLgl0="
+app_id = "uoogio_net"
+access_key = "ttn-account-v2.CxCOkFC9Tz5hrzby0KNF_uO6GRQhs4ah0riVIU2o4W8"
 # Define the JSON message to send to IoT Hub.
 ###TEMPERATURE = 20.0
 #HUMIDITY = 60
@@ -26,6 +34,24 @@ CONNECTION_STRING = "HostName=assignment001.azure-devices.net;DeviceId=device;Sh
 #WINDINTENSITY = 10
 #RAINHEIGHT = 10
 #MSG_TXT = '{{"temperature": {temperature},"humidity": {humidity},"winddirection": {WINDDIRECTION},"windintensity": {WINDINTENSITY},"rainheight": {RAINHEIGHT}}}'
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe("+/devices/+/up")
+
+def uplink_callback(msg, client):
+  print("Received uplink from ", msg.dev_id)
+  print(msg.payload_fields)
+  payload=json.dumps(msg.payload_fields)
+
+  message = Message(payload)
+  client1.send_message(message)
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+    print("ciao")
 
 def iothub_client_init():
     # Create an IoT Hub client
@@ -110,11 +136,14 @@ class Callback:
 
 
 
-if __name__ == '__main__':
-    topic='topic'
-    print ( "IoT Hub Quickstart #1 - Simulated device" )
-    print ( "Press Ctrl-C to exit" )
 
+if __name__ == '__main__':
+    '''
+    topic='topic'
+    '''
+    print ( "Meteo station." )
+    print ( "Press Ctrl-C to exit" )
+    '''
     gateway = Client("gw", port=1885)
     gateway.registerCallback(Callback())
     gateway.connect()
@@ -122,3 +151,11 @@ if __name__ == '__main__':
 
     while True:
         time.sleep(1)
+    '''
+    client1 = iothub_client_init()
+    handler = ttn.HandlerClient(app_id, access_key)
+    mqtt_client = handler.data()
+    mqtt_client.set_uplink_callback(uplink_callback)
+    mqtt_client.connect()
+    time.sleep(120)
+    mqtt_client.close()
